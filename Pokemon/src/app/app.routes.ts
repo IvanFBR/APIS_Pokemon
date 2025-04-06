5624149e0fc27f3fa533c443ecc2ecf4dbe78bba
Ivan Francisco <franciscoivan728@gmail.com>
2025-04-06 10:10:11 -0600

    Ruta lista-pokemones
    Se agrego el componente de "lista-pokemones" y se implementó la funcionalidad del botón, al darle click, te manda al componente anteriormente mencionado

--- Pokemon/src/app/app.routes.ts
+++ Pokemon/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ListaPokemonesComponent } from './lista-pokemones/lista-pokemones.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-//{ path: 'lista-pokemones', component: ListaPokemonesComponent },
+    { path: 'lista-pokemones', component: ListaPokemonesComponent },
     { path: '**', redirectTo: '' }
 ];
---
+++ Pokemon/src/app/lista-pokemones/lista-pokemones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-pokemones',
+  imports: [],
+  templateUrl: './lista-pokemones.component.html',
+  styleUrl: './lista-pokemones.component.css'
+})
+export class ListaPokemonesComponent {
+
+}
