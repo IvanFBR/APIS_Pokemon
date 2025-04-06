5919f7cc0ea680283483385fb3ea08a39920f48e
Ivan Francisco <franciscoivan728@gmail.com>
2025-04-06 11:47:41 -0600

    Implementación Muestras pokemon
    Se implementó la lectura de pokemones mediante el API, usando un diseño de framework, boostrap para los estilos, al igual que imeplementación de paginas

--- Pokemon/src/app/lista-pokemones/lista-pokemones.component.ts
+++ Pokemon/src/app/lista-pokemones/lista-pokemones.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-pokemones',
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './lista-pokemones.component.html',
-  styleUrl: './lista-pokemones.component.css'
+  styleUrls: ['./lista-pokemones.component.css']
 })
-export class ListaPokemonesComponent {
+export class ListaPokemonesComponent implements OnInit {
+  pokemon: any[] = [];
+  pagina = 1;
+  totalPaginas: number = 0;
+  totalPokemones: number = 0;
+  limit: number = 20;
+  cargando: boolean = true;
+
+  constructor(private pokemonService: PokemonService, private router: Router) {}
+
+  ngOnInit() {
+    this.cargarPokemones();
+  }
+
+  cargarPokemones(): void {
+    this.cargando = true;
+    const offset = (this.pagina - 1) * 20;
+
+    this.pokemonService.obtenerPokemon(offset).subscribe((response: any) => {
+      this.totalPokemones = response.count;
+      this.pokemon = response.results;
+      this.totalPaginas = Math.ceil(this.totalPokemones / this.limit);
+      this.cargando = false;
+    });
+  }
+
+  obtenerPokemonNombre(pokemon: any): string {
+    return pokemon.name
+      .replace(/-/g, ' ')
+      .split(' ')
+      .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+
+  cambiarPagina(pagina: number): void {
+    this.pagina = pagina;
+    this.cargarPokemones();
+  }
+
+  filterFalsy(value: any): boolean {
+    return !!value;
+  }
 
 }
